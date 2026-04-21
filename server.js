879e91a2ef684f190745561db94033fc56570326
angeloren92 <angeloren92@gmail.com>
2026-04-21 13:00:04 +0200

    add cors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 const routes = require('./routes/movie_routes');
 const pageNotFound = require('./middleware/pageNotFound');
 const serverError = require('./middleware/serverError');
+const cors = require('cors');
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
@@ -13,6 +14,11 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+
+app.use(cors({
+    origin: process.env.FRONTENDED_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
 app.use(express.static('public'));
 app.use(express.json());
